3594a1633d1eecf9bd253ee161384d01e33da0e9
Sol <sol.oliveira@elevenfinancial.com>
2022-07-10 01:01:06 -0300

    aula 01- home e appBar

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,12 +1,14 @@
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
+import '../home/home_page.dart';
+
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/home/widgets/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../core/app_gradients.dart';
+import '../../core/app_text_styles.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget() 
+  : super(
+    preferredSize: Size.fromHeight(250), 
+    child: Container(      
+      decoration: BoxDecoration(
+        gradient: AppGradients.linear
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text.rich(
+              TextSpan(
+                text: 'Olá, ', 
+                style: AppTextStyles.title,
+                children: [
+                  TextSpan(
+                    text: "Sol Oliveira",
+                    style: AppTextStyles.titleBold,
+                  )
+                ]
+              )
+            ),
+            Container(
+              width: 58,
+              height: 58,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/71611273?v=4"
+                    )
+                    )
+                )
+            )
+          ],
+          ),
+      ),
+    ),
+   ); 
+}
