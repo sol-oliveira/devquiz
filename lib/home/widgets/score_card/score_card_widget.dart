3b83dd3ab973c15100a33489d49d2cd3847545e6
Sol <sol.oliveira@elevenfinancial.com>
2022-07-13 13:06:57 -0300

    aula 1 - chart widget

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,30 +1,44 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/app_colors.dart';
+import '../../../core/app_text_styles.dart';
+import '../chart/chart_widget.dart';
+
 class ScoreCardWidget extends StatelessWidget{
   const ScoreCardWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 136,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(15)
+      ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
         Expanded(
-          flex: 3,
-            child: Container(
-            width: 80,
-            height: 80,
-            color: Colors.red,
-          ),
+          flex: 1,
+          child: ChartWidget()
         ),
         Expanded(
-          flex: 1,
+          flex: 3,
           child:  
             Padding(
               padding: const EdgeInsets.only(left: 24),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Vamos começar"),
-                  Text("Complete os desafios e avance em conhecimento")
+                  Text(
+                    "Vamos começar", 
+                    style: AppTextStyles.heading
+                  ),
+                  Text(
+                    "Complete os desafios e avance em conhecimento",
+                    style: AppTextStyles.body
+                    )
                 ]
               ),
             ),
