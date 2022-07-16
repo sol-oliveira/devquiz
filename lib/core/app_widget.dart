44f6158260889f849c9b94a09a15da6737845561
Sol <sol.oliveira@elevenfinancial.com>
2022-07-15 23:02:14 -0300

    aula 2 challenge Page/ indicator question

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,14 +1,12 @@
-import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:DevQuiz/challenge/challenge_page.dart';
 import 'package:flutter/material.dart';
-
-import '../home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
