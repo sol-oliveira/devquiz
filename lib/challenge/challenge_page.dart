39c6caf9299cc7ae8fb00614f2ca736cd0982e55
Sol <sol.oliveira@elevenfinancial.com>
2022-07-18 23:35:05 -0300

    aula 4 - Next Button Widget

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,4 +1,5 @@
 
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,22 @@ class _ChallengePageState extends State<ChallengePage> {
           child: QuestionIndicatorWidget()
         ),
       ),
-      body: QuizWidget(title: 'O que o Flutter faz em sua totalidade',),
+      body: QuizWidget(
+        title: 'O que o Flutter faz em sua totalidade',
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: 'Pular', ontap: () { },)), 
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: 'Confirmar', ontap: () { },))
+          ]),
+        ),
+      ),
     );
   }
 }
