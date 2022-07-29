ece2300cb017eda4323bfb764045854073edced6
Sol <sol.oliveira@elevenfinancial.com>
2022-07-28 22:36:38 -0300

    aula 4 - add contorller challenge

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,4 +1,5 @@
 
+import 'package:DevQuiz/challenge/challenge_controller.dart';
 import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
@@ -14,7 +15,17 @@ class ChallengePage extends StatefulWidget {
 }
 
 class _ChallengePageState extends State<ChallengePage> {
- 
+ final controller = ChallengeController(); 
+ final pageController = PageController();
+
+  @override
+  void initState() {
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,14 +39,21 @@ class _ChallengePageState extends State<ChallengePage> {
               IconButton(icon: Icon(Icons.close), onPressed: () { 
                 Navigator.pop(context);
              }),
-              BackButton(),
-              QuestionIndicatorWidget(),
+             ValueListenableBuilder(
+              valueListenable: controller.currentPageNotifier,
+              builder: (context, value, _) => QuestionIndicatorWidget(
+                currentPage: controller.currentPage,
+                length: widget.questions.length,
+                ),
+              ),              
             ],
           )
         ),
       ),
-      body: QuizWidget(
-        question: widget.questions[0]
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: widget.questions.map(((e) => QuizWidget(question: e,))).toList()
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
@@ -44,7 +62,11 @@ class _ChallengePageState extends State<ChallengePage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Expanded(child: NextButtonWidget.white(label: 'Pular', ontap: () { },)), 
+              Expanded(child: NextButtonWidget.white(label: 'Pular', ontap: () {
+                pageController.nextPage(
+                  duration: Duration(milliseconds: 100),
+                  curve: Curves.linear);
+               },)), 
               SizedBox(width: 7),
               Expanded(child: NextButtonWidget.green(label: 'Confirmar', ontap: () { },))
           ]),
--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -3,7 +3,12 @@ import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
-  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+  final int currentPage;
+  final int length;
+  const QuestionIndicatorWidget({
+    Key? key,
+    required this.currentPage, 
+    required this.length}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +23,10 @@ class QuestionIndicatorWidget extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Questão 04", style: AppTextStyles.body),
-                    Text("de 10", style: AppTextStyles.body)
+                    Text("Questão $currentPage", 
+                         style: AppTextStyles.body
+                    ),
+                    Text("de $length", style: AppTextStyles.body)
                   ],
                 ),
               ),
@@ -27,7 +34,7 @@ class QuestionIndicatorWidget extends StatelessWidget {
           ),
           SizedBox(height: 16),
           ProgressIndicatorWidget(
-            value: .7,
+            value: currentPage / length,
           )
         ],
       ),
