a56b16be5270b0e0f1dcdc0200cc86b2bd132a6e
Sol <sol.oliveira@elevenfinancial.com>
2022-08-07 19:30:00 -0300

    aula 5 - add page Home root

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -10,7 +10,7 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(Duration(seconds: 2))
-    .then((_) => Navigator.push(
+    .then((_) => Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => HomePage())
       ));
